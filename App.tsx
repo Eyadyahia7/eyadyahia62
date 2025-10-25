a22a97a3a6bfd660f9014f4a307cb0d64d859461
Eyadyahia7 <longliveeyad@gmail.com>
2025-10-25 20:11:48 +0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import WelcomeScreen from './components/WelcomeScreen';
+
+const App: React.FC = () => {
+  return (
+    <main>
+      <WelcomeScreen />
+    </main>
+  );
+};
+
+export default App;
